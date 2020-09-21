ab466aaabab0250bca55ddd2b5124521dc2152d8
Marcel Lindinger <mlindinger18@gmail.com>
2020-09-21 15:20:10 +0200

    initial commit

---
+++ src/firsthue/FirstHue.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package firsthue;
+
+/**
+ *
+ * @author Marcel.Lindinger
+ */
+public class FirstHue {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
